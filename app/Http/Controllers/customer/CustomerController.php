2a906f91764d358bebeba7719a60a856a96a6cb8
Meheady <hmmehedi55@gmail.com>
2022-12-28 02:23:24 +0600

    customer add and delete

--- app/Http/Controllers/customer/CustomerController.php
+++ app/Http/Controllers/customer/CustomerController.php
@@ -10,7 +10,29 @@ class CustomerController extends Controller
 {
     public function customerAll()
     {
-        $allCustomer = Customer::latest();
+        $allCustomer = Customer::latest()->get();;
         return view('backend.customer.all-customer',['data'=>$allCustomer]);
+    }
+
+    public function customerStore(Request $request)
+    {
+        Customer::customerStore($request);
+        return redirect()->back()->with('massage','save successfully');
+    }
+
+    public function customerEdit($id)
+    {
+        $customer = Customer::find($id);
+        return response()->json($customer);
+    }
+
+    public function customerDelete($id)
+    {
+        $customer = Customer::find($id);
+        if (file_exists($customer->image)){
+            unlink($customer->image);
+        }
+        $customer->delete();
+        return response()->json(['massage'=>'Delete success']);
     }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,9 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Customer extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public static $getImgExt;
+    public static $imgName;
+    public static $location;
+    public static $imgUrl;
+    public static function uploadImg($image)
+    {
+        self::$getImgExt = $image->getClientOriginalExtension();
+        self::$imgName = time().'.'.self::$getImgExt;
+        self::$location = "images/customer/";
+        $image->move(self::$location,self::$imgName);
+       return self::$imgUrl = self::$location.self::$imgName;
+    }
+
+    public static function customerStore($request)
+    {
+        $customer = new Customer();
+        $customer->name = $request->name;
+        $customer->phone = $request->phone;
+        $customer->email = $request->email;
+        $customer->address = $request->address;
+        $customer->status = $request->status;
+        $customer->image = self::uploadImg($request->file('image'));
+        $customer->created_by = Auth::user()->id;
+        $customer->save();
+    }
 }
